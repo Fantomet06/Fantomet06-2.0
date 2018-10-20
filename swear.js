3e776ef0f95a9e1e46270681ff1c381d87c0548d
Fantomet06 <44312327+Fantomet06@users.noreply.github.com>
2018-10-20 08:37:16 +0200

    Create swear.js

---
+++ swear.js
@@ -0,0 +1,35 @@
+const Discord = require(`discord.js`);
+const bot = new Discord.Client();
+
+var token = "MzkzNzcwNzE1MjQyOTU0NzUz.DSD_Qg.WEHGWkxdRrgy1H3VYur4FoemcQQ";
+
+bot.on(`ready`, ()=> {
+	console.log(`Started bot as: ${bot.user.tag}!`);
+bot.user.setGame(`Dont swear`);
+	});
+
+const swears = require(`./swears.js`);
+
+bot.on("message", function(message){
+ if (message.author.bot) return;
+ 
+ if (message.guild)
+ {
+	var string = message.content;
+	var lower = string.toLowerCase();
+	for (i = 0; i < 554; i++)
+	{
+ 
+ if (lower.indexOf(swears.list[i]) >= 0)
+	    {
+		 console.log(message.author.username + " said " + lower);
+	      message.delete();
+		  message.reply("Please Don`t say bad words :/")
+		  break;
+		}
+	}
+  }
+
+});
+
+client.login(process.env.BOT_TOKEN);
